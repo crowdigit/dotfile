59de9627037b277b8ebb9ac2625d4e3194b0bd7f
Jake <jake@ad-shield.io>
2025-03-21 17:01:01 +0900

    feature(emacs): add copilot

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -171,3 +171,11 @@
 ;; Highlight line
 (global-hl-line-mode)
 
+;; Copilot
+(use-package copilot
+  :vc (:url "https://github.com/copilot-emacs/copilot.el"
+            :rev :newest
+            :branch "main"))
+(add-hook 'prog-mode-hook 'copilot-mode)
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
